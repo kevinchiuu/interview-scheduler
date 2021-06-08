a29640953464eb99074c15073aeff80bb49441ab
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-06-08 00:33:23 -0400

    button component refactor

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,21 @@
 import React from "react";
+const classnames = require('classnames');
 
-import "components/Button.scss";
+// import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += "button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += "button--danger"
-   }
-   
+   const buttonClass = classnames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+ 
    return (
-      <button 
-         className={buttonClass}
-         onClick={props.onClick}
-         disabled={props.disabled}
-         > 
-         {props.children} 
-      </button>);
-}
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
