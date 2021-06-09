b72d249fd45d107b5c36d1aa2ad35facc7b4e28d
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-06-09 18:23:12 -0400

    add state to appointment component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'components/Button';
 import InterviewerList from 'components/InterviewerList';
 
 export default function Form(props) {
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
+    
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
@@ -12,16 +27,19 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
+            value={name} 
+            onChange={(event) => setName(event.target.value)}
             /*
               This must be a controlled component
             */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
+
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
+          <Button danger onClick={cancel}> Cancel </Button>
           <Button confirm>Save</Button>
         </section>
       </section>
