303efe7dd1f6e94887756ad81a58d2a5e3d6dea1
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-06-09 00:44:44 -0400

    interviewer component refactor

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import "components/InterviewerListItem.scss";
+const classnames = require('classnames');
 
 export default function InterviewListItem(props) {
+
+  const interviewerClass = classnames('interviewers__item', {
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
