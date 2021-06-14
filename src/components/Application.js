a38df7adf13cce8bd4b77b9ee76df1621660fea2
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-06-14 17:19:38 -0400

    display appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import "./Application.scss";
 import DayList from 'components/DayList'
 import Appointment from 'components/Appointment/index';
-
+import { getAppointmentsForDay } from 'helpers/selectors'
 
 export default function Application(props) {
 
@@ -17,19 +17,19 @@ export default function Application(props) {
     interviewers: {}
   })
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
     Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then(([days, appointments, interviewers]) => {
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ]).then((response) => {
       setState(prev => ({
         ...prev,
-        day: days.data,
-        appointments: appointments.data,
-        interviewers: interviewers.data
+        days: response[0].data,
+        appointments: response[1].data,
+        interviewers: response[2].data
       }))
     })
   }, [])
@@ -40,7 +40,7 @@ export default function Application(props) {
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
-        interview={interview}
+        interview={appointment.interview}
       />
     )
   });
