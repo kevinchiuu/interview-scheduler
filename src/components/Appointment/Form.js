ef89284f92e99d12a752099026ad934553eead4c
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-06-17 15:29:08 -0400

    add testing for Appointment and Form components

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
@@ -15,6 +16,16 @@ export default function Form(props) {
   const cancel = () => {
     reset();
     props.onCancel();
+  }
+
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    setError("")
+    props.onSave(name, interviewer);
   }
 
   return (
@@ -29,6 +40,7 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name} 
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
             /*
               This must be a controlled component
             */
@@ -38,9 +50,10 @@ export default function Form(props) {
       </section>
 
       <section className="appointment__card-right">
+      <section className="appointment__validation">{error}</section>
         <section className="appointment__actions">
           <Button danger onClick={cancel}> Cancel </Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
